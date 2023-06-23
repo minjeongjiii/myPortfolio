2af52fe5848ca08543383d45c02fee7fba1d0a41
minjeongjiii <mloy0107@naver.com>
2023-06-23 11:17:21 +0900

    mark01

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-$(function(){
-
+/* scrollTo */
+$(function() {
     const menuBtn = $('.lnb li a')
-    /* const page = $('.container section') */
     menuBtn.click(function(){
         $(window).scrollTo(this.hash || 0, 1000)
     }) 
+});
 
-    console.log(menuBtn)
-    
+/* mousewheel */
+$(function() {
+    $('.container section').mousewheel(function(event,delta){
+      if(delta>0)  { 
+        let prev = $(this).prev().offset().top
+        $('html, body').stop().animate({
+          scrollTop: prev
+        },1000)
+      } else if (delta<0) { 
+        let next = $(this).next().offset().top
+        $('html, body').stop().animate({
+          scrollTop: next
+        },1000)
+      }
+    })
 });
