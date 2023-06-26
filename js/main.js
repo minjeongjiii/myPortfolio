cf85136d5a09e7ba8cb5d20e1441df6eae23cd6c
minjeongjiii <mloy0107@naver.com>
2023-06-26 17:46:16 +0900

    mark01.4

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function() {
 });
 
 /* mousewheel */
-$(function() {
+/* $(function() {
     $('.container section').mousewheel(function(event,delta){
       if(delta>0)  { 
         let prev = $(this).prev().offset().top
@@ -21,4 +21,21 @@ $(function() {
         },1000)
       }
     })
+}); */
+
+document.addEventListener('DOMContentLoaded', function(){
+  
+  /* about scroll event */
+
+  /* const aboutInner = document.getElementById('about_inner');
+  window.addEventListener('scroll', ()=>{
+    let value = window.scrollY;
+    aboutInner.style.width = 700 + value * 5 + 'px';
+    aboutInner.style.height = 500 + value * 3 + 'px';
+    aboutInner.style.right = 135 + value * -1 + 'px';
+    aboutInner.style.bottom = 75 + value * -1 + 'px';
+  });
+  window.addEventListener('scroll','wheel', (e)=>{
+    const delta = e.deltaY * 0.2;
+  }); */
 });
